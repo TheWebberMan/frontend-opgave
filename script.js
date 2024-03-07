c7189e672bebe07c1c6fe3bf899c9fcaab3b98d3
Mike Wiebking <mikewiebking@MBPtilhendeMike.home>
2024-03-07 11:58:33 +0100

    tror det virker. svært at teste på local

--- script.js
+++ script.js
@@ -48,3 +48,23 @@ function resetInterval() {
   stopSlideShow();
   startSlideShow();
 }
+
+
+
+
+
+
+// e-mail service
+
+// window.onload = function() {
+//   document.getElementById('contact-form').addEventListener('submit', function(event) {
+//   event.preventDefault();
+//   // these IDs from the previous steps
+//     emailjs.sendForm('contact_service', 'contact_form', this)
+//     .then(() => {
+//       console.log('SUCCESS!');
+//     }, (error) => {
+//       console.log('FAILED...', error);
+//     });
+//   })
+// }
